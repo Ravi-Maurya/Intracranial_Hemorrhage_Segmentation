0e1fdb7daf5b58c889baaf869ec019688e3f26d9
Ravi Maurya <ravi.rm.maurya@gmail.com>
2020-04-09 23:56:49 +0530

    Flask App V1

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request, redirect, send_from_directory, url_for
+from werkzeug import secure_filename
+import model
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(basedir, 'images')
+
+
+@app.route('/')
+def homepage():
+    return render_template("home.html")
+
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        directory = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        if os.path.exists(directory):
+            return redirect(url_for('run_model', filename=filename))
+        file.save(directory)
+        return redirect(url_for('run_model', filename=filename))
+    return render_template("home.html")
+
+
+@app.route('/<filename>')
+def run_model(filename):
+    nn = model.DeepModel()
+    nn.create()
+    nn.predict(filename)
+    res = nn.result()
+    nn.destroy()
+    del nn
+    return render_template("prediction.html", res=res)
+    # return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -3,40 +3,49 @@ try:
     from cv2 import cv2
     import pydicom
     import keras
-    import keras.backend as K
     from keras.layers import Dense, Dropout
-    from keras.models import Model,model_from_json
+    from keras.models import Model, model_from_json
+    from keras import backend as K
     import efficientnet.keras as efn
+    import plotly
+    import plotly.graph_objs as go
+    import plotly.io as pio
+    import json
+    import gc
 except ImportError:
     from pip._internal import main as pip
-    pip(['install', '--user', 'numpy==1.16.4','opencv-python==4.2.0.32','pydicom==1.4.2','Keras==2.2.5','efficientnet==1.1.0','h5py==2.9.0'])
+
+    pip(['install', '--user', 'numpy==1.16.4', 'opencv-python==4.2.0.32', 'pydicom==1.4.2', 'Keras==2.2.5',
+         'efficientnet==1.1.0', 'h5py==2.9.0'])
     import numpy as np
     from cv2 import cv2
     import pydicom
     import keras
-    import keras.backend as K
     from keras.layers import Dense, Dropout
-    from keras.models import Model,model_from_json
+    from keras.models import Model, model_from_json
     import efficientnet.keras as efn
+    import plotly.graph_objs as go
+    from matplotlib.pyplot import plot
 
 class Transform:
-    def __init__(self,path,SHAPE):
+    def __init__(self, path, shape):
         self.path = path
         self.dcm = pydicom.dcmread(self.path)
-        self.SHAPE = SHAPE
-    
+        self.shape = shape
+
     def correct_dcm(self):
         x = self.dcm.pixel_array + 1000
         px_mode = 4096
-        x[x>=px_mode] = x[x>=px_mode] - px_mode
+        x[x >= px_mode] = x[x >= px_mode] - px_mode
         self.dcm.PixelData = x.tobytes()
         self.dcm.RescaleIntercept = -1000
 
-    def window_image(self, window_center, window_width):    
-        if (self.dcm.BitsStored == 12) and (self.dcm.PixelRepresentation == 0) and (int(self.dcm.RescaleIntercept) > -100):
+    def window_image(self, window_center, window_width):
+        if (self.dcm.BitsStored == 12) and (self.dcm.PixelRepresentation == 0) and (
+                int(self.dcm.RescaleIntercept) > -100):
             self.correct_dcm()
         img = self.dcm.pixel_array * self.dcm.RescaleSlope + self.dcm.RescaleIntercept
-        img = cv2.resize(img, self.SHAPE[:2], interpolation = cv2.INTER_LINEAR)
+        img = cv2.resize(img, self.shape[:2], interpolation=cv2.INTER_LINEAR)
         img_min = window_center - window_width // 2
         img_max = window_center + window_width // 2
         img = np.clip(img, img_min, img_max)
@@ -49,51 +58,64 @@ class Transform:
         brain_img = (brain_img - 0) / 80
         subdural_img = (subdural_img - (-20)) / 200
         soft_img = (soft_img - (-150)) / 380
-        bsb_img = np.array([brain_img, subdural_img, soft_img]).transpose(1,2,0)
+        bsb_img = np.array([brain_img, subdural_img, soft_img]).transpose(1, 2, 0)
         return bsb_img
 
     def read(self):
         try:
             img = self.bsb_window()
         except:
-            img = np.zeros(self.SHAPE)
+            img = np.zeros(self.shape)
         return img
 
+
 class DeepModel:
-    def __init__(self, BATCH_SIZE=16, HEIGHT=256, WEIGHT=256, CHANNEL=3):
-        self.tmp = open('model/model.json','r')
+    def __init__(self, batch_size=16, height=256, width=256, channel=3):
+        self.tmp = open('model/model.json', 'r')
         self.loaded_model = self.tmp.read()
         self.tmp.close()
         self.model = None
-        self.BATCH_SIZE = BATCH_SIZE
-        self.SHAPE = (HEIGHT, WEIGHT, CHANNEL)
+        self.batch_size = batch_size
+        self.shape = (height, width, channel)
         self.IMAGE_DIR = 'images/'
-        self.OUTPUT = ["Any","Epidural","Intraparenchymal","Intraventricular","Subarachnoid","Subdural"]
+        self.OUTPUT = ["Any", "Epidural", "Intraparenchymal", "Intraventricular", "Subarachnoid", "Subdural"]
         self.X = None
         self.Y = None
-    
+
     def create(self):
         self.model = model_from_json(self.loaded_model)
         self.model.load_weights('model/model.h5')
-    
-    def predict(self,ID):
-        self.X = np.empty((self.BATCH_SIZE, *self.SHAPE))
-        self.X[0,] = Transform(self.IMAGE_DIR+ID+".dcm", self.SHAPE).read()
-        self.Y = np.average(self.model.predict(self.X),axis=0)
-    
+
+    def destroy(self):
+        del self.model
+        gc.collect()
+        self.model = None
+        K.clear_session()
+
+    def predict(self, id_name):
+        self.Y = None
+        self.X = None
+        self.id_name = id_name
+        self.X = np.empty((self.batch_size, *self.shape))
+        self.X[0, ] = Transform(self.IMAGE_DIR + self.id_name, self.shape).read()
+        self.Y = np.average(self.model.predict(self.X), axis=0)
+
     def result(self):
-        print("The chance of having {} is {}".format(self.OUTPUT[self.Y.argmax()], self.Y.max()))
-    
+        return self.OUTPUT[self.Y.argmax()]
+
     def probability_table(self):
         for i in range(6):
-            print("{}: {}".format(self.OUTPUT[i],self.Y[i]))
-    
+            print("{}: {}".format(self.OUTPUT[i], self.Y[i]))
+
+    def show_plot(self):
+        fig = go.Figure([go.Bar(x = self.OUTPUT, y = self.Y)])
+        fig.write_html("templates/fig.html")
+        return 'home/ravi/Desktop/GitHub/Intracranial_Hemorrhage_Segmentation/templates/fig.html'
 
 
-if __name__ == "__main__":
-    ID = "ID_000000e27"
-    model = DeepModel(BATCH_SIZE=8)
-    model.create()
-    model.predict(ID)
-    model.result()
-    model.probability_table()
+
+# if __name__=='__main__':
+#     model = DeepModel()
+#     model.create()
+#     model.predict('ID_000000e27')
+#     model.show_plot()
